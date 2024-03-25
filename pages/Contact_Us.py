d3f2c8ded3253b803f2103e60b336a013e5362eb
Anca Dobrin <adobrin208@yahoo.com>
2024-03-25 22:40:48 +0200

    day 23

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
 st.header("Contact Me")
+
+with st.form(key="email_forms"):
+    # adaugam text pe un singur rand
+    user_email = st.text_input("Your email address")
+    # adaugam text pe mai multe randuri
+    message = st.text_area("Your message")
+    # adaugam buton
+    button = st.form_submit_button("Submit")
+    print(button)
+    if button:
+        print("Pressed!")
